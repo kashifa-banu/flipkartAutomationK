b92d99630908be66dffb4128201051fcaff2663d
KashifaBanu <KashifaBanu@DESKTOP-254N2F8>
2019-12-28 00:28:42 +0530

    added junit

--- flipkart/src/test/java/com/flipkart/base/DriverCreation.java
+++ flipkart/src/test/java/com/flipkart/base/DriverCreation.java
@@ -1,8 +1,9 @@
 package com.flipkart.base;
 
+import org.junit.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.annotations.Test;
+
 
 public class DriverCreation {
 	
