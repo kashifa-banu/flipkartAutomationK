42373cf517c2946df3d86f6dd15bd9150458df8c
KashifaBanu <KashifaBanu@DESKTOP-254N2F8>
2020-02-09 10:17:20 +0530

    MavenChrome

--- flipkart/src/test/java/com/flipkart/base/DriverCreation.java
+++ flipkart/src/test/java/com/flipkart/base/DriverCreation.java
@@ -3,6 +3,10 @@ package com.flipkart.base;
 import org.junit.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import io.github.bonigarcia.wdm.ChromeDriverManager;
+import io.github.bonigarcia.wdm.FirefoxDriverManager;
 
 
 public class DriverCreation {
@@ -10,9 +14,13 @@ public class DriverCreation {
 	static WebDriver driver;
 	@Test
 	public void createDriver(){
-		System.setProperty("webdriver.chrome.driver", "./Driver/chromedriver.exe");
+		//System.setProperty("webdriver.chrome.driver", "./Driver/chromedriver.exe");
+		ChromeDriverManager.chromedriver().setup();
+		//FirefoxDriverManager.firefoxdriver().setup();
+		//driver = new FirefoxDriver();
 		driver = new ChromeDriver();
-		driver.get("https://www.flipkar.com/");
+		
+		driver.get("https://www.flipkart.com/");
 		driver.manage().window().maximize();
 	}
 
